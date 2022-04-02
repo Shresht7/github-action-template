a53f668611bad40d3940adc8b451a11886a84bbc
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-04-02 11:38:30 +0530

    Remove GITHUB_WORKSPACE

--- src/config.ts
+++ src/config.ts
@@ -6,12 +6,5 @@ import { inputs, outputs } from './metadata'
 // CONFIG
 // ======
 
-/** GITHUB_WORKSPACE */
-const workspace = process.env.GITHUB_WORKSPACE || ''
-
-if (!workspace) {
-    throw new Error('Invalid GITHUB_WORKSPACE. You need to checkout this repository using the actions/checkout@v3 github-action for the GITHUB_WORKSPACE environment variable.')
-}
-
 /** Input Parameter */
 // export const parameter = core.getInput(inputs.parameter)
